917f15938f08a8f676c25da22db5773af8edddb4
imtiazho <axubair9@gmail.com>
2026-05-14 16:15:27 +0600

    Rider reject functionality completed

--- src/Pages/RiderRequest/RiderRequest.jsx
+++ src/Pages/RiderRequest/RiderRequest.jsx
@@ -74,7 +74,7 @@ const RiderRequest = () => {
             Rider Onboarding Requests
           </h2>
           <p className="text-xs text-gray-400 font-bold uppercase tracking-widest mt-1">
-            ZapShift Fleet Management
+            TradeCen Fleet Management
           </p>
         </div>
         <span className="bg-[#CAEB66] text-[#002B36] px-5 py-2 rounded-2xl text-xs font-black shadow-sm">
@@ -141,18 +141,21 @@ const RiderRequest = () => {
                 </td>
                 <td className="px-8 py-5">
                   <div className="flex justify-end gap-3">
-                    <button className="p-2.5 text-blue-500 hover:bg-blue-50 rounded-xl transition-colors border border-transparent hover:border-blue-100">
+                    <button
+                      disabled
+                      className="p-2.5 text-blue-500 hover:bg-blue-100 rounded-xl transition-colors border border-transparent hover:border-blue-100 cursor-not-allowed"
+                    >
                       <RiEyeLine size={18} />
                     </button>
                     <button
                       onClick={() => handleApprove(rider)}
-                      className="p-2.5 text-green-600 hover:bg-green-50 rounded-xl transition-colors border border-transparent hover:border-green-100"
+                      className="p-2.5 text-green-600 hover:bg-green-50 rounded-xl transition-colors border border-transparent hover:border-green-100 cursor-pointer"
                     >
                       <RiCheckLine size={18} />
                     </button>
                     <button
-                      onClick={() => handleReject(rider)}
-                      className="p-2.5 text-red-500 hover:bg-red-50 rounded-xl transition-colors border border-transparent hover:border-red-100"
+                      onClick={() => handleReject(rider._id)}
+                      className="p-2.5 text-red-500 hover:bg-red-50 rounded-xl transition-colors border border-transparent hover:border-red-100 cursor-pointer"
                     >
                       <RiCloseLine size={18} />
                     </button>
